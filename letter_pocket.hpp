2b13ff547f66e313cdd18e8abcc46701a15114d8
TheoMaillot <theomaillot65@gmail.com>
2025-10-26 10:06:20 +0100

    Deleting Hand class in letter_pocket.hpp

--- letter_pocket.hpp
+++ letter_pocket.hpp
@@ -25,16 +25,4 @@ private:
     int total_letters_;
 };
 
-class Hand {
-public:
-    Hand();                          // main vide
-    void fillFrom(Pocket &pocket);   // remplir jusqu'à HAND_SIZE en tirant du pocket
-    void add(const Letter &l);       // ajoute une lettre (si place)
-    void display(std::ostream &os = std::cout) const noexcept;
-    int size() const noexcept;
-
-private:
-    std::vector<Letter> letters_;    // taille <= HAND_SIZE
-};
-
 #endif
